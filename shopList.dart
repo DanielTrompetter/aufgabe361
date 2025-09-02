e2379fdace817cc6538e164d710e2000cb468bb9
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-02 11:23:50 +0200

    init

---
+++ shopList.dart
@@ -0,0 +1,40 @@
+enum itemCategory
+{
+  meat,
+  vegetables,
+  drinks,
+  personalcare
+}
+
+enum memberGroup
+{
+  family,
+  friends,
+  camping
+}
+
+class shoppingItem
+{
+  int listNumber;         // nummer der Liste in der das Item ist
+  String? name;           // der eigentliche Name
+  bool? bought;           // schon gekauft?
+  itemCategory? category; // die Oberkategorie zu der das Item gehört
+
+  shoppingItem(this.listNumber, this.name, this.bought, this.category);
+}
+
+class shoppingList
+{
+  List<shoppingItem>? items;  //Liste der Items
+  List<member>? members;      //Liste der Miteinkäufer dieser Liste
+  member? owner;              //Derjenige, der diese Liste angelegt hat und verwaltet
+}
+
+class member
+{
+  String? name;               //name
+  String? profilePicture;     //link zum profile picture
+  memberGroup? groupID;       //Die Gruppe zu der dieser Mensch gehört
+
+  member(this.name, this.profilePicture, this.groupID);
+}
